96024c89a2ae9b124b3a3b00746742626d875be0
Wade Fletcher <wbfletch@gmail.com>
2021-01-03 19:09:26 -0500

    Listing 5

--- vec3.h
+++ vec3.h
@@ -42,7 +42,7 @@ public:
     }
 
     double length_squared() const {
-        return e[0] * e[0] + e[1] * e[1] + e[2] * e{ 2 };
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 public:
     double e[3];
@@ -51,5 +51,50 @@ public:
 // Type aliases of vec3
 using point3 = vec3;    // 3D point
 using color = vec3;     // RGB color
+
+
+// vec3 Utility Functions
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+    return (1 / t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u.e[0] * v.e[0]
+        + u.e[1] * v.e[1]
+        + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(
+        u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]
+    );
+}
 
 #endif
