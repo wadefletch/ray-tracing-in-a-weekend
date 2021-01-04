2fe58114e182b057eaa9f1d96d16fae61642ff94
Wade Fletcher <wbfletch@gmail.com>
2021-01-04 17:31:56 -0500

    Typo correction

--- vec3.h
+++ vec3.h
@@ -56,7 +56,7 @@ public:
     bool near_zero() const {
         // Return true if the vector is close to zero in all dimesnsions.
         const auto s = 1e-8;
-        return (fabs(e[0]), s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
     }
 
 
