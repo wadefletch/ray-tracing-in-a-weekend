09273899403e37e97acf719ee6e2e17dae23bf1f
Wade Fletcher <wbfletch@gmail.com>
2021-01-04 17:23:30 -0500

    Listing 47

--- vec3.h
+++ vec3.h
@@ -129,4 +129,8 @@ inline vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
 }
 
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
+}
+
 #endif
