c8f21a9899d7ea5e9d1eb4c06e2ae1c6bb315bdc
Wade Fletcher <wbfletch@gmail.com>
2021-01-04 16:57:08 -0500

    Listing 32

--- vec3.h
+++ vec3.h
@@ -52,6 +52,8 @@ public:
     inline static vec3 random(double min, double max) {
         return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
+
+
 public:
     double e[3];
 };
@@ -107,6 +109,14 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
 }
 
 #endif
