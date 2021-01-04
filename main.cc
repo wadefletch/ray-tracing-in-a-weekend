a7f768d6b8bbaef69c9523cb6fd7bc42bca7b3b7
Wade Fletcher <wbfletch@gmail.com>
2021-01-04 17:27:20 -0500

    Listing 49

--- main.cc
+++ main.cc
@@ -15,8 +15,11 @@ color ray_color(const ray& r, const hittable& world, int depth) {
         return color(0, 0, 0);
 
     if (world.hit(r, 0.001, infinity, rec)) {
-        point3 target = rec.p + rec.normal + random_unit_vector();
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, depth - 1);
+        return color(0, 0, 0);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
